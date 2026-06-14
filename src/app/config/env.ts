0b84c753f1f28be271f199631e8ac6a9acab2318
Aasifur-Rahman <asifur.web@gmail.com>
2026-06-14 21:56:48 +0600

    added admin way

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,28 +1,34 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
 
 interface EnvConfig {
-    PORT: string;
-    DB_URL: string;
-    NODE_ENV: "development" | "production"
+  PORT: string;
+  DB_URL: string;
+  NODE_ENV: "development" | "production";
+  BCRYPT_SALT_ROUND: string;
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV"]
+  const requiredEnvVariables: string[] = [
+    "PORT",
+    "DB_URL",
+    "NODE_ENV",
+    "BCRYPT_SALT_ROUND",
+  ];
 
-    requiredEnvVariables.forEach((key) => {
-        if(!process.env[key]) {
-            throw new Error(`Missing required environment variable ${key}`)
-        }
-    });
-
-    return {
-        PORT: process.env.PORT as string,
-        DB_URL: process.env.DB_URL!,
-        NODE_ENV: process.env.NODE_ENV as "development" | "production"
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing required environment variable ${key}`);
     }
+  });
 
-}
+  return {
+    PORT: process.env.PORT as string,
+    DB_URL: process.env.DB_URL!,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
+    BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+  };
+};
 
-export const envVars : EnvConfig = loadEnvVariables()
+export const envVars: EnvConfig = loadEnvVariables();
--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { StatusCodes } from "http-status-codes";
 import { sendResponse } from "../../utils/sendResponse";
+import { IAdmin } from "./admin.interface";
+import { JwtPayload } from "jsonwebtoken";
 
 const loginAdmin = catchAsync(async (req: Request, res: Response) => {
   const result = await AdminService.loginAdmin(req.body);
@@ -26,3 +28,65 @@ const createInvite = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
+
+const acceptInvite = catchAsync(async(req: Request, res: Response) => {
+  const result = await AdminService.acceptInvite(req.body);
+  
+  sendResponse(res{
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: "Invite Accepted",
+    data: result
+  })
+})
+
+const getAllAdmins = catchAsync(async(req: Request, res: Response) => {
+  const query = req.query;
+  const result = await AdminService.getAllAdmins(query as Record<string,string>)
+  sendResponse(res{
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: "Invite Accepted",
+    data: result
+  })
+})
+
+const getMe = catchAsync(async (req: Request, res: Response) => {
+  const decodedToken = req.user as JwtPayload
+
+  const result = await AdminService.getMe(decodedToken.adminId);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.CREATED,
+    message: "Your profile Retrieved Successfully",
+    data: result.data,
+  });
+
+})
+
+
+const updateAdmin = catchAsync(async(req: Request, res: Response) => {
+  const adminId =req.params.id;
+  const verifiedToken = req.user;
+  const payload: IAdmin = {
+    ...req.body,
+  }
+  const admin = await AdminService.updateAdmin(adminId,payload, verifiedToken as JwtPayload)
+    sendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.CREATED,
+      message: "User updated Successfully",
+      data: admin,
+    });
+})
+
+
+export const AdminControllers = {
+  loginAdmin,
+  createInvite,
+  acceptInvite,
+  getMe,
+  getAllAdmins,
+  updateAdmin,
+}
